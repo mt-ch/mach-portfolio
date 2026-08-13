c21aa4f91edc41d62c87b49575901c871b82fad4
Matt Chan <matt.chan@virtuehealth.co.uk>
2026-08-13 21:25:09 +0800

    Initial UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Matt Chan",
@@ -12,7 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,35 @@ export default async function Home() {
   ]);
 
   return (
-    <main className="relative mx-auto max-w-3xl space-y-16 px-6 py-16">
-      {about && <ReframeHome about={about} projects={projects} />}
-      <ExperienceList entries={experience} />
+    // <main className="relative mx-auto max-w-3xl space-y-16 px-6 py-16">
+    //   {about && <ReframeHome about={about} projects={projects} />}
+    //   <ExperienceList entries={experience} />
+    // </main>
+    <main className="relative">
+      {/* Hero */}
+      <div className="p-md gap-xl bg-brand relative flex flex-col h-124">
+        <h1 className="type-body font-medium text-accent">
+          <span className="text-white">Matthew Chan</span> is a product designer
+          based in San Diego. <br /> Specialising in UX/UI design and digital
+          branding since 2013. <br /> <br /> Available for collaborations and
+          full time roles.
+        </h1>
+
+        <div className="size-sm">
+          <svg
+            className="text-accent"
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            fill="none"
+          >
+            <path
+              d="M 128.945 0 C 199.203 0.508 256 57.617 256 127.994 C 256 198.686 198.692 255.994 128 255.994 C 57.308 255.994 0 198.686 0 127.994 C 0 57.617 56.797 0.509 127.054 0 C 87.725 0.506 56 32.544 56 71.994 C 56 111.759 88.236 143.994 128 143.994 C 167.764 143.994 200 111.759 200 71.994 C 200 32.545 168.274 0.506 128.945 0 Z M 128 47.994 C 141.255 47.994 152 58.739 152 71.994 C 152 85.249 141.255 95.994 128 95.994 C 114.745 95.994 104 85.249 104 71.994 C 104 58.739 114.745 47.994 128 47.994 Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </div>
     </main>
   );
 }
